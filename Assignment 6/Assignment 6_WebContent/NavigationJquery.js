053252a3f3c2bb86e0b5a2a9ce6fa1ef7a29763f
Adit Kulkarni <31707542+Aditkulkarni@users.noreply.github.com>
2019-05-14 21:26:38 -0400

    Assignment 6
    Organized

---
+++ Assignment 6/Assignment 6_WebContent/NavigationJquery.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+$(document).ready(function(){
+
+	/*Function to open Home page on home page tab*/
+	$( "#bhmpg" ).click(function() {
+		document.location.href="AditKulkarniWebsite_Homepage.html";
+	});
+	
+	/*Function to open professional page on professional tab*/
+	$( "#bppg" ).click(function() {
+		document.location.href="Professional.html";
+	});
+	
+	/*Function to open Co-curricular page on Co-curricular tab*/
+	$( "#bccpg" ).click(function() {
+		document.location.href="Co-curricular.html";
+	});
+	/*Function to open ContactME page on Contact Me tab*/
+	$( "#bcmpg" ).click(function() {
+		document.location.href="ContactMe.html";
+	});
+});
